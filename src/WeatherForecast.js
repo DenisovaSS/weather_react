e50d66da32bcb86644bf0d1d5a1c7b109b75b6a8
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-02-01 10:25:17 +0000

    added forecast one day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
-  return (
-    <div className="col-sm-4">
-      <div className="card over">
-        <p className="SignatureForecast">Forecast for next 4 days after</p>
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-        <div className="card-body" id="weather-forecast">
-          <div className="container forecastBody">
-            <div className="row text-center">
-              <div className="col-3">
-                Tue <br />
-                31/1
-              </div>
-              <div className="col-6">
-                <span className="emoji">1°/5°</span>
-              </div>
-              <div className="col-3">
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-                  width="60px"
-                  alt=""
-                  className="forecast_icon"
-                  id="forecast_icon"
-                />
-              </div>
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+
+    //console.log(response.data);
+  }
+  if (loaded) {
+    return (
+      <div className="col-sm-4">
+        <div className="card over">
+          <p className="SignatureForecast">Forecast for next 4 days after</p>
+
+          <div className="card-body" id="weather-forecast">
+            <div className="container forecastBody">
+              <WeatherForecastDay info={forecast[0]} />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "e947cb2640f1db92e6a19005bc43b435";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let IpUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(IpUrl).then(handleResponse);
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,51 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.info.temp.max);
+    return temperature;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.info.temp.min);
+    return temperature;
+  }
+  function day() {
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let date = new Date(props.info.dt * 1000);
+    let day = days[date.getDay()];
+    return day;
+  }
+  function date() {
+    let date = new Date(props.info.dt * 1000);
+    let numberDate = date.getDate();
+    let month = date.getMonth();
+    let monthAll = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let months = monthAll[month];
+    if (months < 10) {
+      months = `0${months}`;
+    }
+    if (numberDate < 10) {
+      numberDate = `0${numberDate}`;
+    }
+    return `${numberDate}/${months} `;
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="row text-center">
+        <div className="col-3">
+          {day()} <br />
+          {date()}
+        </div>
+        <div className="col-6">
+          <span className="emoji">
+            {minTemperature()}°/{maxTemperature()}°
+          </span>
+        </div>
+        <div className="col-3">
+          <WeatherIcon code={props.info.weather[0].icon} size={35} />
+        </div>
+      </div>
+    </div>
+  );
+}
